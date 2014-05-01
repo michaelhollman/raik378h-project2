a4feea08257f5b0bfd5e5730d41f7fa2c1f74aac
clottman <clottman@live.com>
2014-05-01 03:42:57 -0500

    very sleepy attempts. will do better tomorrow.

--- D3_Web/visuals.js
+++ D3_Web/visuals.js
@@ -1,6 +1,6 @@
-﻿var margin = { top: 150, right: 100, bottom: 200, left: 200 };
-var width = 1500 - margin.left - margin.right;
-var height = 1500 - margin.top - margin.bottom;
+﻿var margin = { top: 50, right: 100, bottom: 200, left: 100 };
+var width = 700 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
 
 var chart = d3.select("body").append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -10,6 +10,8 @@ var chart = d3.select("body").append("svg")
 
 var tooltip = chart.append("text")
         .style("visibility", "hidden");
+
+
 
 var rows = d3.csv("outputD3.csv", function (d) {
     var freq = d.frequency;
@@ -21,10 +23,50 @@ var rows = d3.csv("outputD3.csv", function (d) {
         item1: +d.item1,
         item2: +d.item2,
         item3: +d.item3,
-
     };
 }, function (error, rows) {
     console.log(error);
-    console.log(rows);
+    
+    var sentimentScale = d3.scale.linear()
+        .range([-100, height])
+        .domain([-100, 100]);
+    
+    var dayScale = d3.scale.ordinal()
+        .rangePoints([1, 7])
+        .domain(["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]);
 
+    var xAxis = d3.svg.axis()
+        .scale(dayScale)
+        .orient("bottom");
+    
+    var yAxis = d3.svg.axis()
+        .scale(sentimentScale)
+        .orient("left");
+    
+    chart.append("g")
+	    .attr("class", "axis")
+	    .attr("transform", "translate(0, " + 100 + ")")
+	    .call(xAxis);
+
+
+    chart.selectAll("circle")
+        .data(rows)
+    .enter().append("circle")
+        .attr("cx", function (d) { return dayScale(d.frequency[0]); })
+        .attr("cy", function (d) { return sentimentScale(d.frequency[1]) })
+        .attr("r", 3);
+ /*   .on("mouseover", function (d, i) {
+        var tipy = d3.select(this).attr("cy");
+        d3.select(this).style("fill", "#f60");
+        var tipx = d3.select(this).attr("cx");
+        tooltip.attr("x", tipx);
+        tooltip.attr("y", tipy);
+        tooltip.attr("dx", 50);
+        tooltip.attr("dy", -20);
+        tooltip.style("font-size", "40px");
+        tooltip.style("visibility", "visible");
+        tooltip.style("fill", "black");
+        tooltip.text(d.state + ": " + Math.round((d.gdp / d.population) * 1000) / 1000);
+        chart.append("tooltip");
+    })*/
 });
